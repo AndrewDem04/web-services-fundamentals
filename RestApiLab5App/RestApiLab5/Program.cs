a8302d1a08f3048c6ba02d4a9a3aa3901a9dffc1
AndrewDem04 <128223904+AndrewDem04@users.noreply.github.com>
2025-09-07 01:54:46 +0300

    Rest lab5

---
+++ RestApiLab5App/RestApiLab5/Program.cs
@@ -0,0 +1,27 @@
+using RestApiLab5.Models;
+using RestApiLab5.Repo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IBook, InMemBookRepo>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
