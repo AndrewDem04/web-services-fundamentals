053e2c1408ac875577bcaef810e4ee23b8ee589a
AndrewDem04 <128223904+AndrewDem04@users.noreply.github.com>
2025-09-12 14:42:51 +0300

    Consumer-webService

---
+++ SimpleWebService/MyConsumer/MyConsumer/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MyConsumer.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<asp:Label ID="Label1" runat="server" Text="Τιμή"></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;<asp:Label ID="Label2" runat="server" Text="Σύνολο"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calculate" />
+            <br />
+            <br />
+            <asp:Label ID="percentenge" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="Label4" runat="server" Text="= Ποσοστό που υπολογίστηκε" Visible="False"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="time" runat="server"></asp:Label>
+&nbsp;<asp:Label ID="Label6" runat="server" Text="= η ώρα στο server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SimpleWebService/MyConsumer/MyConsumer/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyConsumer
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        // localhost.MyWService είναι ο "τύπος" που δημιούργησε η υπηρεσία
+        // η myWS είναι η μεταβλητή που δηλώνουμε, τύπου localhost.MyWService
+        localhost.WebService myWS = new localhost.WebService();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // η κλήση της μεθόδου ServerTime() δεν χρειάζεται παράμετρους εισόδους
+            // επιστρέφεται string, το οποίο εμφανίζεται μέσω του label lblServerTime
+            time.Text = myWS.ServerTime();
+        } 
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // σε 2 ακέραιες μεταβλητές, υποδεχόμαστε ότι έδωσε ο χρήστης
+            // ως απαραίτητη είσοδο για τη κλήση της μεθόδου CalculatePercentage()
+            int myVal = Convert.ToInt32(TextBox1.Text);
+            int myTot = Convert.ToInt32(TextBox2.Text);
+            // το αποτέλεσμα (ακέραιος) της μεθόδου της υπηρεσίας
+            // μετατρέπεται σε string, για να εμφανιστεί μέσω του label lblPercentage
+            percentenge.Text = Convert.ToString(myWS.CalculatePercentage(myTot,
+            myVal)) + "%";
+            Label4.Visible = true;
+        }
+    }
+}
